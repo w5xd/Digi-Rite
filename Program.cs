5350f2e3172f6f4eec6ae85e4b73034bf3f2e390
Wayne, W5XD. Round Rock <w5xd@writelog.com>
2019-06-16 23:23:06 +0000

    fix null object exception for certain received messages

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace WriteLogDigiRite
         [STAThread]
         static void Main(string[] args)
         {
-            const int UpgradedVersion = 29;  // increment every release
+            const int UpgradedVersion = 30;  // increment every release
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 #if DEBUG
