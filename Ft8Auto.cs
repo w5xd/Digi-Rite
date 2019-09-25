8c383d4d728f8ff5e5773896bd292ef9527e7f9e
Wayne, W5XD. Round Rock <w5xd@writelog.com>
2019-09-25 16:03:15 +0000

    documentation

--- Ft8Auto.cs
+++ Ft8Auto.cs
@@ -5,12 +5,30 @@ using System.Runtime.InteropServices;
 namespace WriteLogDigiRite
 {
     [ComVisible(true), 
-	    // FIXME
-	    // These are the GUID and ProgId that the DigiRite Installer msi at
-        // at writelog.com place in the Windows registry. If you use these, you
-        // replace the installed one when invoked from WriteLog
+	/* FIXME
+        ** If you want to modify DigiRite and simply replace the executable WriteLog invokes,
+        ** you need do NOTHING here.
+        ** If you want to add an alternative that WriteLog invokes, you must change
+        ** the GUID and ProgId below and arrange for your new exe to be COM registered.
+        ** If you don't want to integrate with WriteLog at all, you may eliminate this
+        ** class altogether from this project and figure out how to get DigiRite connected
+        ** to your application your own way.
+        **
+	    ** These are the GUID and ProgId that the DigiRite Installer msi at
+        ** at writelog.com place in the Windows registry. If you use these, you
+        ** replace the installed one when invoked from WriteLog 
+        */
         Guid("4CC973B6-9BFB-3E4B-8157-E55774DD50C0"), 
         ProgId("WriteLog.Ft8Auto")  
+        /* For reference: 
+        ** WriteLog uses the registry key HKEY_LOCAL_MACHINE\SOFTWARE\W5XD\WriteLog\DigitalProductIds
+        ** (on 64b Windows, do the drill with WOW6432Node)
+        ** Name/Value pairs under DigitialProductIds are used by WriteLog to populate its
+        ** right mouse context in its Entry Window. The value is the COM ProgId above.
+        ** the name populates the context menu.
+        ** The executable invoked by using the standard COM ProgId activation process
+        ** WriteLog expects to implement the inteface of the "ComVisible" methods below. 
+        */
         ] 
     public class Ft8Auto
     {
