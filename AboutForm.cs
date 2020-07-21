2537071133a4094ce6732efe4aaaac8122ceacea
Wayne, W5XD. Round Rock <w5xd@writelog.com>
2020-07-21 00:34:49 +0000

    update Help/About

--- AboutForm.cs
+++ AboutForm.cs
@@ -24,7 +24,7 @@ namespace DigiRite
             string version = fvi.ProductVersion; 
             textBoxIntro.Text = "DigiRite" + version  +
                 " Copyright (c) 2020 WriteLog Contesting Software, LLC\r\n\r\n" +
-                "This program is based on wsjtx-2.2.1 which is licensed software.\r\n" +
+                "This program is based on wsjtx-2.2.2 which is licensed software.\r\n" +
                 "See the file COPYING.\r\n\r\n" +
 
 "The algorithms, source code, look-and-feel of WSJT-X and related" +
