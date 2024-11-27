e6c73b13e8f1d4f0b9c4e92d1dd15edba26cab35
Wayne, W5XD. Round Rock <w5xd@writelog.com>
2024-11-27 20:33:55 +0000

    build instructions

---
+++ build-setup.cmd
@@ -0,0 +1,14 @@
+@beta-build-instructions.htm
+@pause "You will be offered 4 files to edit. "
+notepad Digi-XDft\WsjtxBoost.props
+notepad Digi-XDft\XDwsjtFt.props
+notepad AssemblyVersionInfo.cs
+notepad InstallDigiRite\Product.wxs
+pause "We are going to build the various .sln files next"
+pushd Digi-XDft
+msbuild XDft.sln /t:Build /p:Configuration=Release /p:Platform=x86
+msbuild XDft.sln /t:Build /p:Configuration=Release /p:Platform=x86
+popd
+msbuild DigiRite.sln -Restore /t:Build /p:Configuration=Release /p:Platform=x86
+msbuild DigiRite.sln -Restore /t:Build /p:Configuration=Release /p:Platform=x64
+
