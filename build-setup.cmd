9273f7bc979cfe6b83decd690fcf2f1ccb40b3f6
Wayne, W5XD. Round Rock <w5xd@writelog.com>
2024-11-27 21:08:12 +0000

    correct build script

--- build-setup.cmd
+++ build-setup.cmd
@@ -7,7 +7,7 @@ notepad InstallDigiRite\Product.wxs
 pause "We are going to build the various .sln files next"
 pushd Digi-XDft
 msbuild XDft.sln /t:Build /p:Configuration=Release /p:Platform=x86
-msbuild XDft.sln /t:Build /p:Configuration=Release /p:Platform=x86
+msbuild XDft.sln /t:Build /p:Configuration=Release /p:Platform=x64
 popd
 msbuild DigiRite.sln -Restore /t:Build /p:Configuration=Release /p:Platform=x86
 msbuild DigiRite.sln -Restore /t:Build /p:Configuration=Release /p:Platform=x64
